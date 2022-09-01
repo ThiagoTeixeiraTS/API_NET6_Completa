498ee181378c71d5d0e1aadc3af5a6bc8bf42181
Thiago Araujo <arsaoinbdevops@outlook.com>
2022-08-31 21:00:09 -0300

    Add project files.

---
+++ Domain/Services/ServiceMessage.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Interfaces;
+using Domain.Interfaces.InterfaceServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceMessage : IServiceMessage
+    {
+        private readonly IMessage _IMessage;
+
+        public ServiceMessage(IMessage IMessage)
+        {
+            _IMessage = IMessage;
+        }
+
+        public async Task Adicionar(Message obj)
+        {
+            var validTitle = obj.ValidaPropriedadeString(obj.Titulo, "Titulo");
+            if(validTitle)
+            {
+                obj.DataCadastro = DateTime.Now;
+                obj.DataAlteracao = DateTime.Now;
+                obj.Ativo = true;
+                await _IMessage.Add(obj);
+            }
+        }
+
+        public async Task Atualizar(Message obj)
+        {
+            var validTitle = obj.ValidaPropriedadeString(obj.Titulo, "Titulo");
+            if (validTitle)
+            {
+             
+                obj.DataAlteracao = DateTime.Now;
+                await _IMessage.Update(obj);
+            }
+        }
+
+        public async Task<List<Message>> ListarMessageAtivas()
+        {
+            return await _IMessage.ListarMessage(n => n.Ativo);
+        }
+    }
+}
---
+++ Entities/Entities/Notifies.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+
+
+    public class Notifies
+    {
+
+        public Notifies()
+        {
+            Notitycoes = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+        [NotMapped]
+        public string Mensagem { get; set; }
+        [NotMapped]
+        public List<Notifies> Notitycoes { get; set; }
+
+        public bool ValidaPropriedadeString(string valor, string nomePropriedade)
+        {
+            if(string.IsNullOrWhiteSpace(nomePropriedade) || string.IsNullOrWhiteSpace(valor))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                }) ;
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValidaPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(nomePropriedade) || valor < 1 )
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
